c9fc1d854c48e2973ce7acc9e5812c8d512dbb7a
coolcatmeow <trey.dew@outlook.com>
2024-01-16 23:29:26 -0600

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import styles from './App.module.scss';
+import questions from "./questions.json"
+import { Questions } from './types';
+import { useState } from "react"
+import StatBar from './components/statbar';
 
 function App() {
+const allQuestions = questions as Questions
 
-    return <div></div>;
+const [currentQuestionIdx, setCurrentQuestion] = useState(0)
+const [correctAnswers, setCorrectAnswers] = useState(0)
+const [incorrectAnswers, setIncorrectAnswers] = useState(0)
+
+    return <div>
+<StatBar 
+currentQuestion = {currentQuestionIdx + 1}
+totalQuestions = {allQuestions.questions.length}
+correct = {correctAnswers}
+incorrect = {incorrectAnswers}
+ />
+    </div>;
 }
 
 export default App;
