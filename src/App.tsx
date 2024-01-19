1cbb6612d6a28c2e2ab4c4b077d551a68bd6eae3
coolcatmeow <trey.dew@outlook.com>
2024-01-18 19:22:01 -0600

    finsih

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,70 @@
 import styles from './App.module.scss';
-import questions from "./questions.json"
+import questions from './questions.json';
 import { Questions } from './types';
-import { useState } from "react"
+import { useState } from 'react';
 import StatBar from './components/statbar';
 import QuestionComp from './components/Question';
+import Reset from './components/Reset';
+import Answer_module from './components/Answer.module.scss';
+import Classnames from 'classnames';
 
 function App() {
-const allQuestions = questions as Questions;
+    const allQuestions = questions as Questions;
 
-const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
-const [correctAnswers, setCorrectAnswers] = useState(0);
-const [incorrectAnswers, setIncorrectAnswers] = useState(0);
+    const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [incorrectAnswers, setIncorrectAnswers] = useState(0);
 
-const [waitingToAdvance, setWaitingToAdvance] = useState(false);
+    const [waitingToAdvance, setWaitingToAdvance] = useState(false);
 
-const onSubmit = (correct: boolean) => {
-    if(correct) setCorrectAnswers(correctAnswers + 1);
-    else setIncorrectAnswers(incorrectAnswers + 1);
+    const onSubmit = (correct: boolean) => {
+        if (correct) setCorrectAnswers(correctAnswers + 1);
+        else setIncorrectAnswers(incorrectAnswers + 1);
 
-    setWaitingToAdvance(true);
-};
+        setWaitingToAdvance(true);
+    };
 
-const advance = () => { 
-    setWaitingToAdvance(false);
-    setCurrentQuestionIdx(currentQuestionIdx + 1);
-}
+    const advance = () => {
+        setWaitingToAdvance(false);
+        setCurrentQuestionIdx(currentQuestionIdx + 1);
+    };
 
-    return ( 
-        <div>
-            <StatBar 
-                currentQuestion = {currentQuestionIdx + 1}
-                totalQuestions = {allQuestions.questions.length}
-                correct = {correctAnswers}
-                incorrect = {incorrectAnswers}
+    const reset = () => {
+        setCurrentQuestionIdx(0);
+        setCorrectAnswers(0);
+        setIncorrectAnswers(0);
+        setWaitingToAdvance(false);
+    };
+
+    if (currentQuestionIdx >= allQuestions.questions.length)
+        return (
+            <Reset
+                totalQuestions={allQuestions.questions.length}
+                correctQuestions={correctAnswers}
+                onPress={reset}
             />
-            <QuestionComp 
-                question={allQuestions.questions[currentQuestionIdx]} 
+        );
+
+    return (
+        <div>
+            <StatBar
+                currentQuestion={currentQuestionIdx + 1}
+                totalQuestions={allQuestions.questions.length}
+                correct={correctAnswers}
+                incorrect={incorrectAnswers}
+            />
+            <QuestionComp
+                question={allQuestions.questions[currentQuestionIdx]}
                 onSubmit={onSubmit}
             />
-            {waitingToAdvance && <button onClick={advance}>Next Question...</button>}
+            {waitingToAdvance && (
+                <button
+                    onClick={advance}
+                    className={Classnames(Answer_module.answer, styles['next-btn'])}
+                >
+                    Next Question...
+                </button>
+            )}
         </div>
     );
 }
