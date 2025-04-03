73a64e2e9de3f3c5b264a5ad531a5d9847901631
coolcatmeow <trey.dew@outlook.com>
2025-04-03 10:45:42 -0500

    ted

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,6 +1,7 @@
 import { Question } from '../types';
 import Answers from './Answers';
 import Question_module from './Question.module.scss';
+import myVideo from './assets/Videos/Road_work_ahead.mp4';
 
 type Props = {
     question: Question;
@@ -11,6 +12,9 @@ function QuestionComp(props: Props) {
     return (
         <div className={Question_module.app}>
             <h3 className={Question_module.question}>{props.question.question}</h3>
+            <video width="600" controls>
+                 <source src={myVideo} type="video/mp4" />
+            </video>
             <Answers question={props.question} onSubmit={props.onSubmit} />
         </div>
     );
--- src/components/Reset.tsx
+++ src/components/Reset.tsx
@@ -4,7 +4,7 @@ import Classnames from 'classnames';
 import Reset_module from './Reset.module.scss';
 type Props = {
     totalQuestions: number;
-    correctQuestions: number;
+    correctQuestions: number;    
     onPress: () => void;
 };
 
@@ -12,7 +12,7 @@ function Reset(props: Props) {
     return (
         <div className={Reset_module['end-screen']}>
             <h1 className={Reset_module['reset-text']}>
-                You scored: {(props.correctQuestions / props.totalQuestions) * 100}%
+                You scored: {Math.trunc(props.correctQuestions / props.totalQuestions * 100)}%
             </h1>
             <button
                 onClick={props.onPress}
